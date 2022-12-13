dda046c98564c009334f8da15acef2ab1107afa5
Henry-code19 <yawkdjan@gmail.com>
2022-12-13 10:15:26 -0800

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_purchar("_");
+	_purchar("p");
+	_purchar("u");
+	_purchar("t");	
+	_purchar("c");
+	_purchar("h");
+	_purchar("a");
+	_purchar("r");
+	_purchar("\n");
+
+	return (0);
+}
