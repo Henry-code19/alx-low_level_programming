390e0618af79b9ed43596248debe3acef9ce1915
Henry-code19 <yawkdjan@gmail.com>
2022-12-13 16:43:11 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Displays 1 if input is uppercase, otherwise
+ * displays 0
+ *
+ * @c: Input is ASCII code
+ *
+ * Return: 1 if uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	for  ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
