eb437dc9c703346318e0c4b6c0cacc0545e387f4
Henry-code19 <yawkdjan@gmail.com>
2022-12-13 17:32:27 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	_putchar('+');
 	{
-	return (1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n < 0)
-	_putchar('-');
 	{
-	return ('1');
+		_putchar('-');
+		return ('-1');
 	}
 	else
 	{
-	return ('0');
+		return ('0');
 	}
 	_putchar('\n');
 }
