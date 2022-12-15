d3ac1b0e8909680f4019798c395f1948e5a110a3
Henry-code19 <yawkdjan@gmail.com>
2022-12-15 14:06:59 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ *
+ * @c: input is in ASCII code
+ *
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int c);
+{
+	for (c >= 65, c <= 90)
+	{
+	_putchar("c");
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar("\n");
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * File - main.h
+ * Author: Henry-code19
+ * Desc: Header contains prototypes for all functio
+ *         used in C - More functions, more nested loops
+ *         project.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+
+#endif
