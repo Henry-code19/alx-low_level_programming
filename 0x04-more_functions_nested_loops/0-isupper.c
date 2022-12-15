45c376a8dc40e63ab726b44fd03a15f06d7e1759
Henry-code19 <yawkdjan@gmail.com>
2022-12-15 14:24:08 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,7 +8,7 @@
  * Return: 1 if c is uppercase, otherwise 0
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A', c <= 'Z')
 	{
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * File - main.h
  * Author: Henry-code19
- * Desc: Header contains prototypes for all functio
+ * Desc: Header contains prototypes for all functions
  *         used in C - More functions, more nested loops
  *         project.
  */
