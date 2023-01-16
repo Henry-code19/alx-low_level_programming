3836eaa948ae131be5e898e045da5944b87d3bd6
Henry-code19 <yawkdjan@gmail.com>
2023-01-16 07:27:04 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+/*
+ * author: Henry Djan
+ * Desc: header file that defines a function-lke-macro ABS(x)
+ * that computes the absolutevalue of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
