4f12df839e5a17f9ed0110d62d6b16e039522dea
Henry-code19 <yawkdjan@gmail.com>
2023-01-16 07:39:42 -0800

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+/*
+ * Author: Henry Djan
+ * Desc: defines a function-like-macro SUM(x,y) that computes the
+ * sum of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y)) 
+
+#endif
